6ecbc7e0fadcdc372023a6f3fcb03c3cae1b1325
Arturo8aepam <arturo_ochoa@epam.com>
2023-11-06 19:09:35 +0000

    the python code now it works, docker part missing

--- docker/main.py
+++ docker/main.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request,  render_template
+import json
 
 app = Flask(__name__)
 
+# Load the original JSON file
+with open('incomes.json', 'r') as f:
+    original_json = json.load(f)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello():
+    return render_template("index.html")
+
+@app.route("/incomes")
+def get_incomes():
+    return jsonify(original_json)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    # Create a new JSON object to store the form data
+    new_json = {}
+    
+    # Get the form data
+    description = request.form["description"]
+    amount = request.form["amount"]
+
+    new_json["description"] = description
+    new_json["amount"] = amount
+
+    # Create a post request
+    original_json.append(new_json)
+    
+    # Save the updated JSON object to the JSON file
+    with open('incomes.json', 'w') as f:
+        json.dump(original_json, f)
+    
+    # Return a success message
+    return jsonify(original_json)
+        
