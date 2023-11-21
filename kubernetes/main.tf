1067c5d062a4e95082c2e0a5da0378ca4144477b
Arturo8aepam <arturo_ochoa@epam.com>
2023-11-21 17:52:56 +0000

    fix main.tf now the plan works but it failed while applying the plan

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,34 +1,46 @@
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-
-  resource "kubectl_manifest" "test" {
-    yaml_body = <<YAML
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: test-ingress
-  annotations:
-    nginx.ingress.kubernetes.io/rewrite-target: /
-    azure/frontdoor: enabled
-spec:
-  rules:
-  - http:
-      paths:
-      - path: /testpath
-        pathType: "Prefix"
-        backend:
-          serviceName: test
-          servicePort: 80
-YAML
+provider "kubectl" {
+  load_config_file       = false
 }
 
-
-
+resource "kubectl_manifest" "test" {
+    yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+name: hello-kubernetes-ns
+labels:
+    name: hello-kubernetes-ns
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+name: hellow-kubernetes
+namespace: hello-kubernetes-ns
+spec:
+replicas: 3  
+template:
+metadata:
+    labels:
+    app: my-app
+spec:
+    containers:
+    - name: nginx
+    image: paulbouwer/hello-kubernetes
+    ports:
+    - containerPort: 8080
+---
+apiVersion: v1
+kind: Service
+metadata:
+name: hellow-kubernetes–svc
+namespace: hello-kubernetes-ns
+spec:
+type: NodePort
+selector:
+    app: MyApp
+ports:
+- protocol: TCP
+    port: 80
+    targetPort: 80
+YAML  
 }
