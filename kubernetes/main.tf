001925deec83880670e9396447eebf4668978464
Arturo8aepam <arturo_ochoa@epam.com>
2023-11-23 16:17:21 +0000

    yaml files are good, already tested and added in the main.tf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,49 +1,49 @@
 provider "kubectl" {
-    host                   = "https://8080-cs-1020804010302-default.cs-us-central1-brqy.cloudshell.dev"
-    load_config_file       = false
-    config_path            = "/config.yaml" 
-    insecure               = true  
+    #host                   = "https://8080-cs-1020804010302-default.cs-us-central1-brqy.cloudshell.dev"
+    load_config_file       = false 
 }
+
+
 
 resource "kubectl_manifest" "test" {
     yaml_body = <<YAML
 apiVersion: v1
 kind: Namespace
 metadata:
-name: hello-kubernetes-ns
-labels:
     name: hello-kubernetes-ns
 ---
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-name: hellow-kubernetes
-namespace: hello-kubernetes-ns
+  name: hellow-kubernetesdeploynginx
+  namespace: hello-kubernetes-ns
 spec:
-replicas: 3  
-template:
-metadata:
-    labels:
-    app: my-app
-spec:
-    containers:
-    - name: nginx
-    image: paulbouwer/hello-kubernetes
-    ports:
-    - containerPort: 8080
+  replicas: 3
+  selector:
+    matchLabels:
+      app: my-app
+  template:
+    metadata:
+      labels:
+        app: my-app
+    spec:
+      containers:
+        - name: hello-kubernetes
+          image: nginx
+          ports:
+          - containerPort: 8080
 ---
 apiVersion: v1
 kind: Service
 metadata:
-name: hellow-kubernetes–svc
-namespace: hello-kubernetes-ns
+  name: hellow-kubernetesservice
 spec:
-type: NodePort
-selector:
-    app: MyApp
-ports:
-- protocol: TCP
-    port: 80
-    targetPort: 80
+  type: NodePort
+  selector:
+    app: my-app
+  ports:
+    - name: http
+      port: 80
+      targetPort: 80
 YAML  
 }
