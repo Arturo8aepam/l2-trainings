5b7f5927c3195e737fdbc143bdc7d70a5693403b
Arturo8aepam <arturo_ochoa@epam.com>
2024-01-16 15:18:55 +0000

    adding multiconfigfile and modified the main.tf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -6,44 +6,5 @@ provider "kubectl" {
 
 
 resource "kubectl_manifest" "test" {
-    yaml_body = <<YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-    name: hello-kubernetes-ns
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: hellow-kubernetesdeploynginx
-  namespace: hello-kubernetes-ns
-spec:
-  replicas: 3
-  selector:
-    matchLabels:
-      app: my-app
-  template:
-    metadata:
-      labels:
-        app: my-app
-    spec:
-      containers:
-        - name: hello-kubernetes
-          image: nginx
-          ports:
-          - containerPort: 8080
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: hellow-kubernetesservice
-spec:
-  type: NodePort
-  selector:
-    app: my-app
-  ports:
-    - name: http
-      port: 80
-      targetPort: 80
-YAML  
+    yaml_body = file("multiconfigfile.yaml")
 }
