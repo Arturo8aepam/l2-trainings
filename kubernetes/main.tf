08598349decbf1677e5f4718c764692541c95560
Arturo8aepam <arturo_ochoa@epam.com>
2024-01-16 22:25:00 +0000

    now it works!

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,10 +1,18 @@
 provider "kubectl" {
-    load_config_file       = false
-    host = "https://192.168.49.2:8443"  # Use Minikube's IP and port
+    load_config_file       = true
+    #host = "https://192.168.49.2:8443"  # Use Minikube's IP and port
     #token = "YOUR_KUBECONFIG_TOKEN"  # Replace with the token from your Minikube kubeconfig maybe not needed
-    cluster_ca_certificate = file("/google/minikube/.minikube/ca.crt")  # Assuming kubeconfig is in this path
+    #cluster_ca_certificate = file("/google/minikube/.minikube/ca.crt")  # Assuming kubeconfig is in this path
 }
 
-resource "kubectl_manifest" "test" {
-    yaml_body = file("./multiconfigfile.yaml")
+resource "kubectl_manifest" "test-namespace" {
+    yaml_body = file("./namespace.yaml")
+}
+
+resource "kubectl_manifest" "test-deployment" {
+    yaml_body = file("./deployment.yaml")
+}
+
+resource "kubectl_manifest" "test-service" {
+    yaml_body = file("./service.yaml")
 }
