bc7c2645421d5e02538907b11027ba0ab22bd3d5
Arturo8aepam <arturo_ochoa@epam.com>
2024-01-16 15:49:24 +0000

    trying w/ minikube

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,10 +1,10 @@
 provider "kubectl" {
-    #host                   = "https://8080-cs-1020804010302-default.cs-us-central1-brqy.cloudshell.dev"
-    load_config_file       = false 
+    load_config_file       = false
+    host = "https://192.168.49.2:8443"  # Use Minikube's IP and port
+    #token = "YOUR_KUBECONFIG_TOKEN"  # Replace with the token from your Minikube kubeconfig maybe not needed
+    cluster_ca_certificate = base64decode(file("/home/user/.kube/config"))  # Assuming kubeconfig is in this path
 }
 
-
-
 resource "kubectl_manifest" "test" {
-    yaml_body = file("multiconfigfile.yaml")
+    yaml_body = file("./multiconfigfile.yaml")
 }
