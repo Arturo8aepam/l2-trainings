baef5e183a10d26807314bf6c74bd3ee0ecf989a
Arturo8aepam <arturo_ochoa@epam.com>
2024-01-16 16:18:37 +0000

    next try use kubernetes provider

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -2,7 +2,7 @@ provider "kubectl" {
     load_config_file       = false
     host = "https://192.168.49.2:8443"  # Use Minikube's IP and port
     #token = "YOUR_KUBECONFIG_TOKEN"  # Replace with the token from your Minikube kubeconfig maybe not needed
-    cluster_ca_certificate = base64decode(file("/home/user/.kube/config"))  # Assuming kubeconfig is in this path
+    cluster_ca_certificate = file("/google/minikube/.minikube/ca.crt")  # Assuming kubeconfig is in this path
 }
 
 resource "kubectl_manifest" "test" {
